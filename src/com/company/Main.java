031c49b45242aeb79dd9f7264f7bb0ac01597cb2
kkaaban <krutovdmitrij0@gmail.com>
2022-02-16 18:24:11 +0200

    innit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,8 @@
 package com.company;
 
-import javax.crypto.spec.RC2ParameterSpec;
-import java.util.Random;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         HashTable hashTable = new HashTable(10);
         System.out.println("Создана хэш-таблица размером 10. Добавление прямоугольников: ");
         for (int i = 0; i < 6; i++) {
